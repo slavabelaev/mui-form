eeb5337202ccc94eab24a323d0c9ffcf5a054fcc
slavabelaev <slavabelaev@users.noreply.github.com>
2022-11-25 22:25:54 +0000

    deploy: 05716cb266d59607d463c6b8218897f1eb07207b

--- static/js/67.7fdfb1cd.chunk.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkmui_form=self.webpackChunkmui_form||[]).push([[67],{30067:function(s){s.exports=JSON.parse('{"$schema":"http://json-schema.org/draft-07/schema","type":"string","format":"color"}')}}]);
